2e400d56b1e60bd3cffdb00bc9fd234b2ce805ad
Nikolay Kim <fafhrd91@gmail.com>
2017-10-29 21:42:47 -0700

    basic example

---
+++ examples/chat.rs
@@ -0,0 +1,63 @@
+//! Test server for sockjs-protcol functiona tests
+extern crate actix;
+extern crate actix_web;
+extern crate sockjs;
+extern crate env_logger;
+
+use std::fs::File;
+use std::io::Read;
+
+use actix_web::*;
+use actix::prelude::*;
+use sockjs::{Message, Session, SockJSManager};
+
+struct Chat;
+
+impl Actor for Chat {
+    type Context = sockjs::SockJSContext<Self>;
+}
+
+impl Default for Chat {
+    fn default() -> Chat {
+        Chat
+    }
+}
+
+impl Session for Chat {}
+
+impl Handler<Message> for Chat {
+    fn handle(&mut self, msg: Message, ctx: &mut sockjs::SockJSContext<Self>)
+              -> Response<Self, Message>
+    {
+        ctx.send(msg);
+        Self::empty()
+    }
+}
+
+
+fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    let _ = env_logger::init();
+
+    let sys = actix::System::new("sockjs-chat");
+
+    let sm: SyncAddress<_> = SockJSManager::<Chat>::start_default();
+
+    HttpServer::new(
+        Application::default("/")
+            .middleware(Logger::new(None))
+            .route_handler(
+                "/sockjs/", sockjs::SockJS::<Chat, _>::new(sm.clone()))
+            .resource("/", |r| r.handler(Method::GET, |_, _, _| {
+                let mut file = File::open("examples/chat.html")?;
+                let mut content = String::new();
+                file.read_to_string(&mut content)?;
+
+                Ok(httpcodes::HTTPOk
+                   .builder()
+                   .body(content)?)
+            })))
+        .serve::<_, ()>("127.0.0.1:8080").unwrap();
+
+    let _ = sys.run();
+}
